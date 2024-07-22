f12ce5b1e42a2506ac13fd46964b61f01c585404
CaioLems <caiog3216@gmail.com>
2024-07-22 19:45:01 -0400

    CONVÊNIENCIA DIGITAL

---
+++ js/mercado.js
@@ -0,0 +1,9 @@
+function finalizar(){
+        Swal.fire({
+                position: "top-center",
+                icon: "success",
+                title: "Compra finalizada,Obrigado e volte sempre!!",
+                showConfirmButton: false,
+                timer: 1500
+              });
+}
